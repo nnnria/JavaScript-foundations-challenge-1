ebf78acd1542bc7eabbfe4687f97eb0ca767e197
Núria Ruiz Font <nuriaruizfont@gmail.com>
2023-10-17 21:06:11 +0200

    finish exercise 01

--- js/exercise-01.js
+++ js/exercise-01.js
@@ -7,5 +7,31 @@
       Log 'name' value.
     5. On the next line, call function 'updateValue' value '28'
       Log 'name' value.
-
 */
+
+// 1. Create a variable called 'name' that points to a string containing your name
+let name = "Núria Ruiz Font";
+
+// 2. Define a function 'updateValue' that receives a value and updates the 'name' variable
+function updateValue(value) {
+  name = value;
+}
+
+// 3. Call function 'updateValue' with your favorite musician's name
+updateValue("La Zowi");
+
+// Log 'name' value
+console.log(name); // Output: "Favorite Musician"
+
+// 4. Call function 'updateValue' with the value 'true'
+updateValue(true);
+
+// Log 'name' value
+console.log(name); // Output: true
+
+// 5. Call function 'updateValue' with the value '28'
+updateValue(28);
+
+// Log 'name' value
+console.log(name); // Output: 28
+
