f72ccce672a60e852d359baa1303b6543f5746b6
Núria Ruiz Font <nuriaruizfont@gmail.com>
2023-10-17 20:25:29 +0200

    finish the exercise 00.

--- js/exercise-00.js
+++ js/exercise-00.js
@@ -8,3 +8,24 @@
 
    Log out each variable to the terminal at the end.
 */
+// Constant String
+const myString = "Hello, World";
+
+// Constant Number
+const myNumber = 42;
+
+// Constant Boolean
+const myBoolean = true;
+
+// Constant Null
+const myNull = null;
+
+// Constant Undefined
+const myUndefined = undefined;
+
+// Log the variables to the terminal
+console.log("Constant String:", myString);
+console.log("Constant Number:", myNumber);
+console.log("Constant Boolean:", myBoolean);
+console.log("Constant Null:", myNull);
+console.log("Constant Undefined:", myUndefined);
