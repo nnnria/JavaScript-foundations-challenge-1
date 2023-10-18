32dcf84f19fa5fa192d1e9b10c61f55a70156ffa
Núria Ruiz Font <nuriaruizfont@gmail.com>
2023-10-18 20:37:23 +0200

    finish exercise 02

--- js/exercise-02.js
+++ js/exercise-02.js
@@ -3,5 +3,28 @@
     2. On a separate line, assign the variable the String: "Moo!"
     3. Log out this variable to the terminal using console.log
     
-    Repeat the above steps but try using a const variable. Comment this new part of code and explain what is happening.
+    Repeat the above steps but try using a const variable.
+    Comment this new part of code and explain what is happening.
 */
+
+// 1. Declare a variable using let with any name you like, but without a value.
+//let nuria;
+
+// 2. On a separate line, assign the variable the String: "Moo!"
+//nuria = "Moo";
+
+// 3. Log out this variable to the terminal using console.log
+
+//console.log(nuria);
+
+// Repeat the above steps but try using a const variable. 
+//Comment this new part of code and explain what is happening.
+
+const nuria;
+ruiz = "Mooo";
+console.log(nuria);
+
+//When you look in the browser console it warns you Missing initializer in const declaration.
+//The difference between let and const is that let allows subsequent assignments 
+//and does not require an initial value in the declaration, while const requires 
+//an initial value in the declaration and does not allow subsequent reassignments.
