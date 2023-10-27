3510a01be8b6468382dc07cd5eee1aacddd75069
Núria Ruiz Font <nuriaruizfont@gmail.com>
2023-10-27 16:35:42 +0200

    fix the mistake in exercise-03

--- js/exercise-03.js
+++ js/exercise-03.js
@@ -3,8 +3,11 @@
         Use typeof to ckech the types for each of the main 5 primitive types (string, number, boolean, null, undefined)
     2. Log the result of any function call.
 */
-function getTypeOf(value) {
+function getTypeOf(value){
     return typeof(value);
-  }
-
-console.log(getTypeOf(value));          
+}
+console.log(getTypeOf(true));
+console.log(getTypeOf(13));   
+console.log(getTypeOf("LoreIpsum")); 
+console.log(getTypeOf()); // console.log(getTypeOf(undefined));
+console.log(getTypeOf(null));      
